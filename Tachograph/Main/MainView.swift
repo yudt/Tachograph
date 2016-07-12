ec1af2c69bfb3110dcbf2f7dcefcf319e24bef18
Ethan <dfhwangqiang@163.com>
2016-07-12 11:20:28 +0800

    增加地图动画，缺横竖屏适配

--- Tachograph/Main/MainView.swift
+++ Tachograph/Main/MainView.swift
@@ -33,9 +33,10 @@ class MainView: UIView {
     private let captureView = CaptureView()
     
     /**
-     *  遮盖物视图
+     *  导航条
      */
-    private let overlayView = OverlayView()
+    let tabbar : TabbarView = TabbarView()
+
     
     
     /**
@@ -46,35 +47,49 @@ class MainView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame : frame)
-        self.backgroundColor = UIColor.blackColor()
+        backgroundColor = UIColor.blackColor()
         mapView.frame = frame
         captureView.frame = frame
-        overlayView.frame = frame
-        
-        self.addSubview(captureView)
-        self.addSubview(mapView)
-        self.addSubview(overlayView)
-        self.addSubview(rectButton)
-        mapView.frame = CGRectMake(0, 0, mapviewWH, mapviewWH)
-        mapView.layer.cornerRadius = 15
-        mapView.layer.masksToBounds = true
+        rectButton.frame = CGRectMake(0, 0, mapviewWH, mapviewWH)
+        rectButton.layer.cornerRadius = 15
+        rectButton.layer.masksToBounds = true
         rectButton.alpha = 0.4
+        rectButton.addTarget(self, action: #selector(MainView.changeMapCaptureFrameValueAction(_:)), forControlEvents: .TouchUpInside)
+        rectButton.selected = true // true map.frame == rectButton   |||| false captureView.frame == rectButton
+        // 添加初始化的小视图位置
+        if bounds.size.width > bounds.size.height {
+            // 当横向 在右下角
+            rectButton.frame = CGRectMake(bounds.size.width - rectButton.frame.size.width - margin,bounds.size.height - rectButton.frame.size.height - margin, rectButton.bounds.size.width, rectButton.bounds.size.height)
+            
+        }else{
+            rectButton.frame = CGRectMake(bounds.size.width - rectButton.frame.size.width - margin, bounds.size.height - rectButton.frame.size.height - margin - tabbarH, rectButton.bounds.size.width, rectButton.bounds.size.height)
+        }
         
+        if rectButton.selected {
+            addSubview(captureView)
+            addSubview(mapView)
+        }else{
+            addSubview(mapView)
+            addSubview(captureView)
+        }
+        changeMapCaptureFrameValueAction(rectButton)
+        addSubview(tabbar)
+        addSubview(rectButton)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MainView.deviceOrientationDidChangeNotificationAction(_:)), name: UIApplicationDidChangeStatusBarOrientationNotification, object: nil)
         // delegate tabbarButtonAction
-        for obj in overlayView.tabbar.buttonArr {
+        for obj in tabbar.buttonArr {
             if obj.isKindOfClass(UIButton) {
             obj.addTarget(self, action: #selector(MainView.buttonAction(_:)), forControlEvents: .TouchUpInside)
             }
         }
         
-     MSGLog(Message: "是否显示小地图\(AppConfigure.sharedConfigure().showSmallMap)")
-        
-        let appconfig = AppConfigure.sharedConfigure()
-        appconfig.showSmallMap = false
-        appconfig.synchronize()
-        MSGLog(Message: "\(appconfig.showSmallMap)")
-        
-        
+        // 配置相关 ，后续加上
+//     MSGLog(Message: "是否显示小地图\(AppConfigure.sharedConfigure().showSmallMap)")
+//        
+//        let appconfig = AppConfigure.sharedConfigure()
+//        appconfig.showSmallMap = false
+//        appconfig.synchronize()
+//        MSGLog(Message: "\(appconfig.showSmallMap)")
     }
     
     
@@ -85,17 +100,17 @@ class MainView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         // 横屏
-        if self.bounds.size.width > self.bounds.size.height {
-            // 当横向 在右下角
-            mapView.frame = CGRectMake(self.bounds.size.width - mapView.frame.size.width - margin, self.bounds.size.height - mapView.frame.size.height - margin, mapView.bounds.size.width, mapView.bounds.size.height)
-
+        if bounds.size.width > bounds.size.height {
+            tabbar.frame = CGRectMake(0, 0, tabbarH, bounds.size.height)
         }else{
-            mapView.frame = CGRectMake(self.bounds.size.width - mapView.frame.size.width - margin, self.bounds.size.height - mapView.frame.size.height - margin - tabbarH, mapView.bounds.size.width, mapView.bounds.size.height)
+            tabbar.frame = CGRectMake(0, bounds.size.height - tabbarH, bounds.size.width, tabbarH)
         }
         
-        rectButton.frame = mapView.frame
-        captureView.frame = self.bounds
-        overlayView.frame = self.bounds
+        bringSubviewToFront(rectButton)
+    }
+    
+    deinit{
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
     
     /*
@@ -111,10 +126,64 @@ class MainView: UIView {
 
 extension MainView{
     
-    @objc private func buttonAction(btn : UIButton){
+    @objc private func buttonAction(btn : UIButton) {
 
         btn.selected = !btn.selected
         delegate?.MainViewOverlayTabbarClicked?(btn.tag - 6122,btn.selected)
+    }
+    
+    @objc private func changeMapCaptureFrameValueAction(btn : UIButton) {
+        
+        btn.selected = !btn.selected
+        
+        UIView.animateWithDuration(0.3, animations: {
+            
+                if btn.selected{
+                    self.mapView.frame = self.bounds
+                    self.mapView.layer.cornerRadius = 0
+                    self.mapView.layer.masksToBounds = false
+                }else{
+                    self.captureView.frame = self.bounds
+                    self.captureView.layer.cornerRadius = 0
+                    self.captureView.layer.masksToBounds = false
+                }
+            
+            }) { (over) in
+                
+                if btn.selected{
+                    self.captureView.frame = btn.frame
+                    self.captureView.layer.cornerRadius = 15
+                    self.captureView.layer.masksToBounds = true
+                    self.sendSubviewToBack(self.mapView)
+                }else{
+                    self.mapView.frame = btn.frame
+                    self.mapView.layer.cornerRadius = 15
+                    self.mapView.layer.masksToBounds = true
+                    self.sendSubviewToBack(self.captureView)
+                }
+                
+        }
+    }
+    
+    @objc private func deviceOrientationDidChangeNotificationAction(noti : NSNotification){
+        
+        if rectButton.selected{
+            self.mapView.frame = self.bounds
+            self.mapView.layer.cornerRadius = 0
+            self.mapView.layer.masksToBounds = false
+            self.captureView.frame = rectButton.frame
+            self.captureView.layer.cornerRadius = 15
+            self.captureView.layer.masksToBounds = true
+            self.sendSubviewToBack(self.mapView)
+        }else{
+            self.captureView.frame = self.bounds
+            self.captureView.layer.cornerRadius = 0
+            self.captureView.layer.masksToBounds = false
+            self.mapView.frame = rectButton.frame
+            self.mapView.layer.cornerRadius = 15
+            self.mapView.layer.masksToBounds = true
+            self.sendSubviewToBack(self.captureView)
+        }
     }
     
 }
