bef085f3a14abf5016dda0e0020dee15af9b67ff
wangqiang <wangqiang@mapbar.com>
2016-07-15 11:45:00 +0800

    集成 'FreeStreamer', '~> 3.5.8'

--- Podfile
+++ Podfile
@@ -6,6 +6,7 @@ target 'Tachograph' do
 	pod 'SDWebImage', '~>3.7'
 	pod 'SVProgressHUD','~>2.0.3'
 	pod 'Reachability', '~> 3.2'
+    pod 'FreeStreamer', '~> 3.5.8'
 		#高德地图
 	pod 'AMap3DMap' ,'~>3.3.2' #3D地图SDK
 		#pod 'AMap2DMap' #2D地图SDK (2D和3D不能同时使⽤用)
--- Pods/Target Support Files/Pods-Tachograph/Pods-Tachograph-frameworks.sh
+++ Pods/Target Support Files/Pods-Tachograph/Pods-Tachograph-frameworks.sh
@@ -85,12 +85,14 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FreeStreamer/FreeStreamer.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Reachability/Reachability.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SVProgressHUD/SVProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FreeStreamer/FreeStreamer.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Reachability/Reachability.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SVProgressHUD/SVProgressHUD.framework"
